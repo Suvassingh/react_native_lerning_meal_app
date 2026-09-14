80c870e5ce99b9f232c31b469f9fea0064671ba4
suvas <ss5138275@gmail.com>
2026-09-14 07:44:40 +0545

    build:project setup and catrgory display

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import CategoryScreen from "../screens/CategoryScreen";
+export default function App() {
+  return (
+ 
+      <CategoryScreen />
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
